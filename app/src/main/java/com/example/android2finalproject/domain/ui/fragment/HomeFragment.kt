ffe07d6c1b478a056e458e3b8ec1e6b80e5a74a4
Assanalikhan <210103042@stu.sdu.edu.kz>
2024-05-13 21:01:39 +0500

    Hide keyboard

--- app/src/main/java/com/example/android2finalproject/domain/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/android2finalproject/domain/ui/fragment/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.example.android2finalproject.domain.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android2finalproject.R
@@ -31,5 +35,17 @@ class HomeFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(context)
         val adapter = WeatherAdapter(this, weatherList)
         recyclerView.adapter = adapter
+
+        binding.otherInputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                hideKeyboard(view)
+                true } else { false} }
+        binding.imageButton.setOnClickListener {
+            hideKeyboard(it)
+        }
+    }
+    fun hideKeyboard(view: View) {
+        val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
